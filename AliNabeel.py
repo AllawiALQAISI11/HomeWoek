65d63fa49f3d9e22ea239ef4012a0a555681b23d
AllawiALQAISI11 <60901062+AllawiALQAISI11@users.noreply.github.com>
2025-10-20 21:10:52 +0300

    Update AliNabeel.py

--- AliNabeel.py
+++ AliNabeel.py
@@ -1,19 +1,32 @@
-import hashlib
+##its work in windows Only
+import socket
 import requests
-pwd = input("Enter Pass To Cheack :")
-def check_password(password):
-    sha = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    passchr5 = sha[:5] 
-    bakpass = sha[5:]
-    resp = requests.get(f"https://api.pwnedpasswords.com/range/{passchr5}")
-    resp.raise_for_status()
-    for line in resp.text.splitlines():
-        offcpass, count = line.split(':')
-        if offcpass == bakpass:
-            return int(count)
-    return 0
-times = check_password(pwd)
-if times:
-    print(f"Dont Use it Leaked {times} times")
-else:
-    print("Not Leaked Coool:)")
+import subprocess
+import re
+class SystemInfo:
+    def get_local_ip(self):
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            local_ip = s.getsockname()[0]
+            s.close()
+            return local_ip
+            
+    def get_public_ip(self):
+            response = requests.get('https://api.ipify.org')
+            return response.text
+            
+    def get_mac_address(self):
+                command = "getmac"
+                result = subprocess.check_output(command, shell=True, encoding='utf-8')
+                return result
+                
+    def get_wifi_password(self, wifi_name):
+        command = f'netsh wlan show profile name="{wifi_name}" key=clear' 
+        result = subprocess.check_output(command, shell=True, encoding='utf-8')
+        key_content_match = re.search(r"Key Content\s+:\s+(.*)", result)
+        if key_content_match:
+            password = key_content_match.group(1).strip()
+            return password
+wifi_name_input = input("Enter Name Of Wifi : ") 
+collector = SystemInfo()
+print(f"Local IP : {collector.get_local_ip()}\nPublic IP : {collector.get_public_ip()}\n{collector.get_mac_address()}\nWifi Pass: {collector.get_wifi_password(wifi_name_input)}")
